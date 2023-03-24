2f507f921b897cf08da2994577f509a3bbbbb4c4
Zaccaria <emanuele.zaccaria@fabrick.com>
2023-03-24 18:43:17 +0100

    Star tow new function

--- Dog.js
+++ Dog.js
@@ -9,8 +9,8 @@ class Dog {
         const { name, avatar, age, bio } = this
         return `                
         <img class="photo" src="${avatar}" alt="dog photo">
-        <img class="badge hidden" data-like="like" src="/images/badge-like.png" alt="like">
-        <img class="badge hidden" data-like="dislike" src="/images/badge-nope.png" alt="dislike">
+        <img class="badge hidden" src="/images/badge-like.png" alt="like">
+        <img class="badge hidden" src="/images/badge-nope.png" alt="dislike">
         <div class="text">
             <div class="title"> ${name}, ${age}</div>
             <div class="description"> ${bio} </div>
--- index.js
+++ index.js
@@ -3,8 +3,23 @@ import Dog from "./Dog.js";
 
 const newDog = new Dog(dogs[2])
 
+function handleLike () {
+    console.log("Like");
+}
+
+function handleDislike () {
+    console.log("dislike");
+
+}
+
 function render () {
     document.getElementById("image-container").innerHTML = newDog.dogHtml()
 }
 
+const dislikeButton = document.getElementById("nope")
+const likeButton = document.getElementById("like")
+
+
+likeButton.addEventListener("click", handleLike)
+dislikeButton.addEventListener("click", handleDislike)
 render();
